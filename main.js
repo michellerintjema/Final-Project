753fe967a6583b1c76df4e0d8018ae49e9586f33
Michelle Rintjema <michellerintjema@Spare-MBP-13.lan>
2018-03-18 10:49:59 +1100

    Content update, images added, css changes

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 console.log('connected');
 
-$("#side-menu-burger").click(function(event) {
+$(".sideMenu_burger").click(function(event) {
   event.preventDefault()
-  $("#hide-side-menu-span").toggleClass("hide_side_menu_span")
-  $("#show-menu-on-click").toggleClass("show_pop_out_menu");
+  $(".mainBanner").toggleClass("hide_mobile_mainBanner")
+  $("#hide-side-menu-span").toggleClass("hide_sideMenu_menuSpan")
+  $("#show-menu-on-click").toggleClass("show_sideMenu_popOut");
+  $(".mainBanner_title").toggleClass("hide_mobile_mainBanner_title")
+  $(".mainNav_downArrow").toggleClass("hide_mobile_mainNav_downArrow")
 });
 
-$("#gallery, .mainTitle").click(function(event) {
+$("#sideMenu-popOut-galleries, .mainBanner_title, .mainNav_downArrow").click(function(event) {
+  togglePopOutMenu();
   $('html, body').animate({
-    scrollTop: $("#gallery-section").offset().top
+    scrollTop: $("#galleries").offset().top
   }, 1000);
 });
 
-$("#about, #contact").click(function(event) {
+$("#sideMenu-popOut-about").click(function(event) {
+  togglePopOutMenu();
   $('html, body').animate({
-    scrollTop: $("#about-contact-section").offset().top
+    scrollTop: $("#about-contact-section").position().top
   }, 1000);
 });
+
+$("#sideMenu-popOut-contact").click(function(event) {
+  togglePopOutMenu();
+  $('html, body').animate({
+    scrollTop: $("#footer").position().top
+  }, 1000);
+});
+
+function togglePopOutMenu() {
+  $(".hide_mobile_mainBanner").removeClass("hide_mobile_mainBanner")
+  $("#show-menu-on-click").removeClass("show_sideMenu_popOut");
+  $("#hide-side-menu-span").removeClass("hide_sideMenu_menuSpan")
+  $(".hide_mobile_mainBanner_title").removeClass("hide_mobile_mainBanner_title")
+  $(".hide_mobile_mainNav_downArrow").removeClass("hide_mobile_mainNav_downArrow")
+}
